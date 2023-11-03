66bfa81c090ddab895a782fc34bf50daffdea502
felix k <131535833+felixsunyk@users.noreply.github.com>
2023-11-03 02:26:31 +0100

    u

--- srcipt.js
+++ srcipt.js
@@ -1,31 +1,45 @@
 "use scrict"
 
+const page1 = document.querySelector(".index-page");
+const page2 = document.querySelector(".index1home");
+const indexEmail = document.getElementById("loginemail");
+const indexPassword = document.getElementById("loginpassword");
+const login = document.getElementById("login");
+// welcome message on the main page
+const nameSpan = document.getElementById("nameSpan");
+// sign up page
+ const nameInput = document.getElementById("signName");
+// const signUp = document.getElementById("sign")
+
 // JavaScript (script.js):
 // home page javascript
 window.addEventListener("load", () => {
   // Automatically animate from page 1 to page 2
   setTimeout(() => {
-    const page1 = document.querySelector(".index-page");
     page1.style.transform = "translateX(-100%)";
     setTimeout(() => {
-        const page2 = document.querySelector(".index1home");
-        page2.style.transform = "translateX(0%)";
-      }, );
-    }, 1000);
+      page2.style.transform = "translateX(0%)";
+    });
+  });
 });
-// JavaScript (script.js):
-// javascript
-// window.addEventListener("load", () => {
-//   Automatically animate from page 1 to page 2
-//   const page1 = document.querySelector(".page1");
-//   setTimeout(() => {
-//     page1.style.transform = "translateX(-100%)";
-    
-//     Show the second page after a delay (3 seconds)
-//     setTimeout(() => {
-//       const page2 = document.querySelector(".page2");
-//       page2.style.transform = "translateX(0%)";
-//     }, 3000);
-//   }, 3000);
-// });
+
+// login page
+
+login.addEventListener("click", function () {
+  if (
+    indexEmail.value === "felix@gmail.com" &&
+    indexPassword.value === "11111"
+  ) {
+    window.location.href = "Reset.html";
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
+});
+
+const getInputName = () => {
+  const name = nameInput.value;
+    // Updateting the content of the span with the name
+    nameSpan.textContent = name;
+}
+
 
